a2cfe46e03ba1fb45289890e2e086ba1b5e546ca
Chris Charles <w0481147@nscc.ca>
2024-11-20 00:18:59 -0400

    Set up basic form, program can now take user input and pass it to ai model and get a response

--- app.py
+++ app.py
@@ -3,17 +3,29 @@ from transformers import pipeline, AutoTokenizer, T5ForConditionalGeneration
 
 app = Flask(__name__)
 
-qa_model = pipeline("question-answering","distilbert/distilbert-base-cased-distilled-squad")
+# model used: https://huggingface.co/grammarly/coedit-large
+
+tokenizer = AutoTokenizer.from_pretrained("grammarly/coedit-large")
+model = T5ForConditionalGeneration.from_pretrained("grammarly/coedit-large")
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    tokenizer = AutoTokenizer.from_pretrained("grammarly/coedit-large")
-    model = T5ForConditionalGeneration.from_pretrained("grammarly/coedit-large")
-    input_text = 'Fix grammatical errors in this sentence: When I grow up, I start to understand what he said is quite right.'
-    input_ids = tokenizer(input_text, return_tensors="pt").input_ids
-    outputs = model.generate(input_ids, max_length=256)
-    edited_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return render_template("home.html", input_text = input_text, edited_text = edited_text)
+    if request.method =="GET":
+        return render_template("home.html")
+    else:
+        # Retrieve form input text:
+        input_text = request.form["input_text"]
+
+        # Tokenize the input text to prepare it for the model
+        input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+
+        # Use the model to generate an output based on the input tokens
+        outputs = model.generate(input_ids, max_length=256)
+
+        # Decode the model's output back into human-readable text
+        edited_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+        return render_template("home.html", input_text = input_text, edited_text = edited_text)
 
 
 
